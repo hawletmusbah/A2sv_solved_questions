9cc585b5d5b683f7df67a2f1dae8454a8dda3c14
hawlet musbah <hawletmusbah00@gmail.com>
2026-04-15 22:15:38 +0300

    Add solution for Find the Duplicate Number

---
+++ leetcode/0287-Find-the-Duplicate-Number/0287-Find-the-Duplicate-Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            correct_index = nums[i] - 1
+            if nums[i] != nums[correct_index]:
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+            else:
+                i += 1
+        return nums[-1]
