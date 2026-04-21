e24f0f743ac7923d01a88929493e797cf4f95431
hawlet musbah <hawletmusbah00@gmail.com>
2026-04-21 18:25:46 +0300

    Add solution for Sort Colors

--- leetcode/0075-Sort-Colors/0075-Sort-Colors.py
+++ leetcode/0075-Sort-Colors/0075-Sort-Colors.py
@@ -3,21 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-     
-        low = 0
-        mid = 0
-        high = len(nums) - 1
+        zero = nums.count(0)
+        one = nums.count(1)
+        two = nums.count(2)
+        init = zero
+        mid = zero + one
+        last = zero + one + two
         
-        while mid <= high:
-            if nums[mid] == 0:
-                # Swap current element with the low pointer
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
-            elif nums[mid] == 1:
-                # Move to the next element
-                mid += 1
-            else: # nums[mid] == 2
-                # Swap current element with the high pointer
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+        for i in range(0,init):
+            nums[i] = 0
+        
+        for j in range(init,mid):
+            nums[j] = 1
+        
+        for k in range(mid,last):
+            nums[k] = 2
