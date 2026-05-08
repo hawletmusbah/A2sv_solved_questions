bf1b37023848de63eeb51a348cc6073dbee13521
hawlet musbah <hawletmusbah00@gmail.com>
2026-05-08 10:38:19 +0300

    Add solution for N-Queens II

---
+++ leetcode/0052-N-Queens-II/0052-N-Queens-II.py
@@ -0,0 +1,43 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def isSafe(row, col, board):
+            c_col ,c_row = col,row
+            while c_row >= 0 and c_col >= 0 :
+                if board[c_row][c_col] == 'Q':
+                    return False
+                c_row -= 1
+                c_col -= 1
+
+
+            c_col ,c_row = col,row
+            while c_col >= 0 :
+                if board[c_row][c_col] == 'Q':
+                    return False
+                c_col -= 1
+
+            c_col ,c_row = col,row
+            while c_col >= 0 and c_row < n :
+                if board[c_row][c_col] == 'Q':
+                    return False
+                c_row += 1
+                c_col -= 1
+            return True
+
+        def solve(col, board, ans, n):
+            #base case 
+            if col == n:
+                ans.append(["".join(row) for row in board ])
+                return 
+
+            for row in range(n):
+                if isSafe(row,col,board):
+                    board[row][col] = 'Q'
+                    solve(col + 1,board,ans,n)
+
+                    board[row][col] = '.'
+
+        ans = []
+        # Initialize an n x n board with '.'
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        solve(0, board, ans, n)
+        return len(ans)
